6dfd1f36a01bded2416ed0a1d487f564ffa3b994
adindaviya <adindaalifviya@gmail.com>
2024-03-06 23:19:59 +0700

    Kumpul Jurnal 03

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList list1 = new SinglyLinkedList();
+
+        list1.tambahDepan(new Buku("A", "b", 2024));
+        list1.tambahDepan(new Buku("A", "b", 2024));
+        list1.tambahDepan(new Buku("A", "b", 2024));
+        list1.tambahDepan(new Buku("A", "b", 2024));
+        list1.tambahDepan(new Buku("A", "b", 2024));
+        list1.tambahDepan(new Buku("A", "b", 2024));
+
+        list1.print();
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+JURNAL 03
+
+Nama: Adinda Alif Viya
+
+NIM: 607062300052
+
+Kelas: D3IF-47-04 
